d3bce9d9c2410346a62ce3e1ac75e0a9cad8230e
hamilli <130806529+hamilli@users.noreply.github.com>
2024-05-28 15:44:39 -0700

    Add files via upload

---
+++ excercises/en/solution_01_05.py
@@ -0,0 +1,9 @@
+# Import pandas 
+import pandas as pd
+
+# Read in the data
+hockey_players = pd.read_csv('data/canucks.csv')
+
+
+# Display the dataframe
+hockey_players.head()
---
+++ excercises/en/solution_05_04.py
@@ -0,0 +1,16 @@
+exercise = 'burpees'
+
+# Make if, elif, and else statements
+
+if exercise == 'lunges':
+    reps = 20
+elif exercise == 'squats':
+    reps = 25
+elif exercise == 'burpees':
+    reps = 15
+else:
+    reps = 10
+
+# Display the value of reps
+
+reps
